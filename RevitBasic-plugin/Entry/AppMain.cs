501448294bbfe6b73cbd313cd821fb24400d4971
tachkov.maksim <tachkov.maksim@gmail.com>
2024-05-19 19:10:28 +0300

    initial commit

---
+++ RevitBasic-plugin/Entry/AppMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace RevitBasic_plugin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class AppMain : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //Define a reference Object to accept the pick result
+            Reference pickedref = null;
+
+            //Pick a group
+            Selection sel = uiapp.ActiveUIDocument.Selection;
+            pickedref = sel.PickObject(ObjectType.Element, "Please select a group");
+            Element elem = doc.GetElement(pickedref);
+            Group group = elem as Group;
+
+            //Pick point
+            XYZ point = sel.PickPoint("Please pick a point to place group");
+
+            //Place the group
+            Transaction trans = new Transaction(doc);
+            trans.Start("Lab");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            trans.Commit();
+
+
+            return Result.Succeeded;
+        }
+    }
+}
