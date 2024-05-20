bdd045d24724fca5c95ad0e3c1012969f831675f
Tachkov Maksim <Tachkov.Maksim@ide-spb.com>
2024-05-20 18:02:25 +0300

    Hello revit

---
+++ RevitBasic-plugin/AppMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+
+namespace RevitBasicPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class AppMain : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            TaskDialog myDialog = new TaskDialog("Hello, Revit!");
+            myDialog.Show();
+
+
+            return Result.Succeeded;
+        }
+    }
+}
